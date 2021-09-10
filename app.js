6627817a97cee795613259167510ab54f2b948d1
Jonah Cost <costjonah@gmail.com>
2021-09-10 00:55:42 -0400

    Altered CSS styling. Progressed logic for AI bot's move selection

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var modalDisplay = false;
 
 var tableData = document.getElementById('table');
 var rows = document.getElementsByClassName('row');
+var cells = document.getElementsByClassName('col');
 
 var modal = document.getElementById('alert-modal');
 var span = document.getElementsByClassName("close")[0];
@@ -24,6 +25,9 @@ window.onclick = function (event) {
 var exCount = 0;
 var ohCount = 0;
 
+var currentBoardState = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
+var currentXCount = 0;
+
 var winningCombos = [
   [0, 1, 2],
   [3, 4, 5],
@@ -40,6 +44,7 @@ var checkWin = (boardObj) => {
   var exChoices = [];
   var ohChoices = [];
   for (var key in boardObj) {
+    currentBoardState[Number(boardObj[key].idx)] = boardObj[key].choice;
     if (boardObj[key].choice === 'X') {
       exChoices.push(Number(boardObj[key].idx));
     } else if (boardObj[key].choice === 'O') {
@@ -81,11 +86,16 @@ var stateTracker = () => {
       }
     }
   }
+  for (var key in boardState) {
+    currentBoardState[Number(boardState[key].idx)] = boardState[key].choice;
+  }
+  // console.log(currentBoardState);
   return boardState;
 };
 
 // signals end of round and winner if one
 var endGame = (winner) => {
+  endFlag = true;
   turnFlag = true;
   if (winner !== 'DRAW') {
     modal.style.display = "block";
@@ -100,7 +110,10 @@ var endGame = (winner) => {
 
 // functionality for new game button, does not refresh browser
 restartGame = () => {
+  endFlag = false;
   turnFlag = true;
+  currentBoardState = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
+  currentXCount = 0;
   Array.from(document.getElementsByClassName('col')).forEach(cellText => {
     cellText.textContent = '';
   })
@@ -109,6 +122,103 @@ restartGame = () => {
       cell.classList.remove('X');
       cell.classList.remove('O');
     }
+  }
+};
+
+var openCells = () => {
+  var remains = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
+  var checking = stateTracker();
+  for (var key in checking) {
+    remains = remains.filter(item => item !== checking[key].idx)
+  }
+  remains = randomize(remains);
+  return remains;
+};
+
+var randomize = (arr) => {
+  for (var i = arr.length - 1; i > 0; i--) {
+    var random = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[random]] = [arr[random], arr[i]];
+  }
+  return arr;
+};
+
+// --- incomplete bot function. bot randomizes but doesnt block users moves yet ---
+var botPicksSpot = () => {
+  // assign var to first index of randomized board state
+  // var freeSpots = openCells()[0];
+  // // instantiate empty container
+  // var results = [];
+  // // instantiate shallow copy of possible winning combinations
+  // var combos = winningCombos.slice();
+  // // iterate through combos matrix
+  // for (var k = 0; k < combos.length; k++) {
+  //   // iterate through current level of matrix
+  //   for (var y = 0; y < combos[k].length; y++) {
+  //     // check if current iteration's value is string X
+  //     if (currentBoardState[combos[k][y]] === 'X') {
+  //       // push to container
+  //       results.push(combos[k][y]);
+  //     }
+  //   }
+  // }
+  // // create unique array from container's values
+  // results = Array.from(new Set(results));
+
+  // // iterate through winning combination - keep track of index and it's value
+  // for (var [index, combo] of combos.entries()) {
+  //   // initialize array from filtering open spots from combos
+  //   var spotFilter = combo.filter((moves) => !results.includes(Number(moves)));
+  //   // check if total 'X's on board's current state is equal to two
+  //   if (currentXCount === 2) {
+  //       // check if last remaining value
+  //       if (spotFilter.length === 1) {
+  //         // if 'X' or 'O' does not exist before move
+  //         if (spotFilter[0] !== 'X' && spotFilter[0] !== 'O') {
+  //           // reassign var to return to the single value of array
+  //           freeSpots = spotFilter[0];
+
+  //          }
+
+  //     }
+  //   }
+    // } else if (currentXCount > 2) {
+    //   console.log('COMBO', combo)
+    //   spotFilter = combo.filter((moves) => !currentBoardState.includes(Number(moves)))
+    //   console.log('LINE198', spotFilter)
+    //   if (spotFilter.length < 3) {
+    //     if (spotFilter.length === 1) {
+    //       if (spotFilter[0] !== 'X' && spotFilter[0] !== 'O') {
+    //         freeSpots = spotFilter[0]
+    //         if (spotFilter.length === 0) {
+    //           if (openCells()[0] !== 'X' && openCells()[0] !== 'O') {
+    //             freeSpots = openCells()[0]
+    //           }
+    //         }
+    //       }
+    //     }
+    //   }
+
+    // }
+  // }
+  // return value
+  // return freeSpots;
+  return openCells()[0];
+};
+
+spotTaken = () => {
+  var toSlice = openCells();
+  return toSlice.slice(0, 1);
+};
+
+var bot = () => {
+  var botMove = botPicksSpot();
+  if (botMove !== undefined) {
+    spotTaken();
+    var oh = document.createTextNode("O");
+    document.getElementById(botMove).appendChild(oh);
+    document.getElementById(botMove).classList.add('O');
+    turnFlag = true;
   }
 };
 
@@ -122,21 +232,26 @@ var renderChoice = () => {
         modalContent.innerHTML = "You cannot make this move";
         modal.style.display = "block";
         modalDisplay = true;
-      } else if (turnFlag === true) {
+      } else if (turnFlag === true && endFlag === false) {
         var ex = document.createTextNode("X");
         table.appendChild(ex);
         table.classList.add('X');
+        currentXCount++;
         turnFlag = false;
-        tempRes = stateTracker();
-        check = checkWin(tempRes);
+        setTimeout(() => {
+          bot(); // comment out to disable
+          tempRes = stateTracker();
+          check = checkWin(tempRes);
+        }, 400);
 
-      } else if (turnFlag === false) {
-        var oh = document.createTextNode("O");
-        table.appendChild(oh);
-        table.classList.add('O');
-        turnFlag = true;
-        tempRes = stateTracker();
-        check = checkWin(tempRes);
+        // ---- code to play without bot ----
+        //   } else if (turnFlag === false) {
+        //   var oh = document.createTextNode("O");
+        //   table.appendChild(oh);
+        //   table.classList.add('O');
+        //   turnFlag = true;
+        //   tempRes = stateTracker();
+        //   check = checkWin(tempRes);
       }
     });
   }
